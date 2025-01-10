05181ce7b8eaea6af0ebdb1ebea22682d5e36bc8
b0vv0d <atimirsin862@gmail.com>
2025-01-10 18:22:37 +0500

    100125.1

--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
+from database.models import BankCard
 from django.shortcuts import render
 
 def home(request):
+    user = request.user
+    if not user.is_authenticated:  # Проверка на аутентификацию
+        card = None
+    else:
+        try:
+            card = BankCard.objects.get(user=user)
+        except BankCard.DoesNotExist:
+            card = None
     return render(
         request,
         'home/home.html',
         {
             'is_authenticated': False,
+            'user': user,
+            'card': card
         }
     )
--- site_auth/urls.py
+++ site_auth/urls.py
@@ -5,6 +5,8 @@ app_name = 'auth'
 
 urlpatterns = [
     path('', views.auth, name='auth'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('reg/', views.reg, name='reg'),
+    path('verify/', views.verify, name='verify'),
 ]
